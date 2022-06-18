54700eaf4b9a72a8b4f592ed428ae98c0bba76a5
Tom Smolin <tomsmo016@gmail.com>
2022-06-18 15:20:46 +0300

    MDP WET begininng

--- MDP/value_and_policy_iteration.py
+++ MDP/value_and_policy_iteration.py
@@ -2,6 +2,42 @@ from copy import deepcopy
 import numpy as np
 
 
+def get_probability(mdp, next_state, state, action_chosen):
+    action_made = action_chosen
+    for action in mdp.actions:
+        if mdp.step(state, action) == next_state:
+            action_made = action
+            break
+
+    action_made_idx = ["UP", "DOWN", "RIGHT", "LEFT"].index(action_made)
+    return mdp.transition_function[action_chosen][action_made_idx]
+
+
+def reward(mdp, state):
+    if mdp.board[state[0]][state[1]] == 'WALL':
+        return None
+    return float(mdp.board[state[0]][state[1]])
+
+
+def get_states_list(mdp):
+    states = []
+    for r in range(mdp.num_row):
+        for c in range(mdp.num_col):
+            if mdp.board[r][c] != 'WALL':
+                    states.append((r, c))
+    return states
+
+
+def possible_next_states(mdp, state):
+    next_states = []
+    for action in mdp.actions:
+        next_state = mdp.step(state, action)
+        if next_state not in next_states:
+            next_states.append(next_state)
+
+    return next_states
+
+
 def value_iteration(mdp, U_init, epsilon=10 ** (-3)):
     # TODO:
     # Given the mdp, the initial utility of each state - U_init,
@@ -11,7 +47,37 @@ def value_iteration(mdp, U_init, epsilon=10 ** (-3)):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    # raise NotImplementedError
+    U_curr = U_init
+    states = get_states_list(mdp)
+    row, col = 0, 1
+    while True:
+        U, delta = U_curr, 0
+        for state in states:
+
+            bellman_rhs_max_options = []
+            next_states = possible_next_states(mdp, state)
+
+            for act_chosen in mdp.actions:
+                p_u_elements = []
+                for next_state in next_states:
+                    p_u_elements.append(get_probability(mdp, next_state, state, act_chosen) * U[state[row]][state[col]])
+                action_sum = sum(p_u_elements)
+                bellman_rhs_max_options.append(action_sum)
+
+            U_curr[state[row]][state[col]] = reward(mdp, state) + mdp.gamma * max(bellman_rhs_max_options)
+            if abs(U_curr[state[row]][state[col]] - U[state[row]][state[col]]) > delta:
+                delta = abs(U_curr[state[row]][state[col]] - U[state[row]][state[col]])
+
+        if mdp.gamma == 1 and delta == 0:
+            break
+
+        if mdp.gamma != 1 and delta < ((epsilon * (1 - mdp.gamma))/mdp.gamma):
+            break
+
+
+
+    return U
     # ========================
 
 
